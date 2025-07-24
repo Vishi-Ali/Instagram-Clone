ff520f961b29ddc48c5e6c8e089e9b8abae225fc
Vishesh Vigh <vigh.vishesh@gmail.com>
2025-07-24 23:36:52 +0530

    Fix Deploy Issues

--- instagram-clone/eslint.config.mjs
+++ instagram-clone/eslint.config.mjs
@@ -5,12 +5,18 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
+const compat = new FlatCompat({ baseDirectory: __dirname });
+
+export default compat.config({
+  extends: ["next/core-web-vitals", "next/typescript"],
+  rules: {
+    "no-unused-expressions": ["error", {
+      allowShortCircuit: true,
+      allowTernary: true
+    }],
+    "@typescript-eslint/no-unused-expressions": ["error", {
+      allowShortCircuit: true,
+      allowTernary: true
+    }]
+  }
 });
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
