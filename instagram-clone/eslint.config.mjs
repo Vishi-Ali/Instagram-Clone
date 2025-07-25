84006a8543b3c46c88d03c2cf8a2b321990371fd
Vishesh Vigh <vigh.vishesh@gmail.com>
2025-07-25 07:26:10 +0530

    Revert "Fix Deploy Issues"
    This reverts commit ff520f961b29ddc48c5e6c8e089e9b8abae225fc.

--- instagram-clone/eslint.config.mjs
+++ instagram-clone/eslint.config.mjs
@@ -5,18 +5,12 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({ baseDirectory: __dirname });
-
-export default compat.config({
-  extends: ["next/core-web-vitals", "next/typescript"],
-  rules: {
-    "no-unused-expressions": ["error", {
-      allowShortCircuit: true,
-      allowTernary: true
-    }],
-    "@typescript-eslint/no-unused-expressions": ["error", {
-      allowShortCircuit: true,
-      allowTernary: true
-    }]
-  }
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
 });
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+];
+
+export default eslintConfig;
